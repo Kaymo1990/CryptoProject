038326f84e0de382ec2f9c501cf5205dad1e2884
Kay <khurrammohammed1990@gmail.com>
2019-10-15 16:18:52 +0100

    date map method all working now

--- models/crypto.rb
+++ models/crypto.rb
@@ -36,6 +36,24 @@ request["x-rapidapi-host"] = 'coingecko.p.rapidapi.com'
 request["x-rapidapi-key"] = '2e00d77e9bmshb4658adca133189p181bfejsnaa6ab9b43cc8'
 
 response = http.request(request)
-JSON.parse(response.read_body)
+JSON.parse(response.read_body)["market_data"]["current_price"]["usd"]
 
+end
+
+def timeConversion
+    Time.new.strftime("%d-%m-%Y")
+end
+
+def dateMap(days)
+        oneDAY = 86400
+        mappedDates = []
+        dayCount = 0
+
+        while dayCount < days 
+            mappedDates << (Time.now - (oneDAY * dayCount)).strftime("%d-%m-%Y")
+            dayCount+=1
+        end
+
+        return mappedDates
+    end
 end
