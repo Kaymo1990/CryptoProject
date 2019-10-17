eccaa5d70beb3c60fb7b4daf8c9479804f30d3b0
Kay <khurrammohammed1990@gmail.com>
2019-10-17 13:43:47 +0100

    updated with additional tradestatement tests

--- spec/tradestatement_spec.rb
+++ spec/tradestatement_spec.rb
@@ -16,10 +16,17 @@ describe TradeStatement do
             expect(subject.tradeRecommendationStatements(input)).to eq expectedOutput
         end
 
-        it 'should return a statement indicating low volatility' do
-            expectedOutput =  "There is a low volatility of 0 compared to the benchmark of 0.3.
-            This implies the price has stayed relatively stable, a sign of support! "
-            input = "lowvolatility"
+        it 'should return a statement indicating trade above the average' do
+            expectedOutput =  "The current price of BTC is above the 100 day moving average.
+            This means it is trading above what it has historically traded at. "
+            input = "aboveaverage"
+            expect(subject.tradeRecommendationStatements(input)).to eq expectedOutput
+        end
+        
+        it 'should return a statement indicating trade below the average' do
+            expectedOutput =  "The current price of BTC is above the 100 day moving average.
+            This means it is trading above what it has historically traded at. "
+            input = "aboveaverage"
             expect(subject.tradeRecommendationStatements(input)).to eq expectedOutput
         end
 
