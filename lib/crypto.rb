b8dd4a1db00dfde2c8d22c95dd7af995c8ae1f33
Kay <khurrammohammed1990@gmail.com>
2019-10-19 01:56:03 +0100

    update api key

--- lib/crypto.rb
+++ lib/crypto.rb
@@ -31,7 +31,7 @@ class Crypto
 
         request = Net::HTTP::Get.new(url)
         request["x-rapidapi-host"] = 'coingecko.p.rapidapi.com'
-        request["x-rapidapi-key"] = '2e00d77e9bmshb4658adca133189p181bfejsnaa6ab9b43cc8'
+        request["x-rapidapi-key"] = '2c19478a9dmsh82b87dc0ef20995p129153jsnad6290d366b5'
 
         response = http.request(request)
         return JSON.parse(response.read_body)["market_data"]["current_price"]["usd"]
