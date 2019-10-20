06559d4ab0dde8ae9127c7c8d75b64885c1357ef
Kay <khurrammohammed1990@gmail.com>
2019-10-20 17:00:43 +0100

    updated with checking search result

--- spec/features/testing_search_spec.rb
+++ spec/features/testing_search_spec.rb
@@ -4,4 +4,11 @@ feature 'cryptosearch' do
         click_on 'Back to homepage'
         expect(page).to have_current_path('/')
     end
+
+    scenario 'entering bitcoin into search will take user to the analysis page' do
+        visit('/cryptosearch')
+        fill_in 'coin', with: 'bitcoin'
+        click_on 'Submit'
+        expect(page).to have_content 'The current price of bitcoin is'
+    end
 end
